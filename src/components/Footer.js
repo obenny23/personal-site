e2ac32d2b8289867daebe3e001b8402f2ccac24e
Benigno Ortega <b_ortega@umail.ucsb.edu>
2022-10-08 19:59:19 -0700

    bo - simple structure in place

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from '../logo.svg';
+const Footer = () => {
+    return(
+        <div className="social">
+            <footer>
+               <h2> footer</h2>
+            </footer>
+        </div>
+    )
+    
+};
+
+export default Footer;
